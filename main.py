80f914b04d1f481a2a2c71da4dd1474cc0d9a03a
David Christy <Davidchristy520@gmail.com>
2017-11-14 16:37:04 -0700

    adding voice detection

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from MySQL import MySQL
 
 from VerbHandler import VerbHandler
 
+from ListenerInstance import ListenerInstance
+
 class ReuseAddrTCPServer(socketserver.TCPServer):
     allow_reuse_address = True
 
 def main():
 	PORT = 5000
+	listenerInstance = ListenerInstance()
+
+	
 	httpd = ReuseAddrTCPServer(("", PORT), VerbHandler)
 	print("Listening on port: {}".format(PORT))
 	httpd.serve_forever()
+
 
 	
 
